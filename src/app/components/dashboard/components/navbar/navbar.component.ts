5b018a0772936e4f1c11b173a03f86069748706a
unknown <mahmoud-hossam@outlook.com>
2024-05-06 20:11:39 +0300

    handle collapsed button into navbar

--- src/app/components/dashboard/components/navbar/navbar.component.ts
+++ src/app/components/dashboard/components/navbar/navbar.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../../sevices/auth-service.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  collapsed = true;
 
   constructor(public authService: AuthService, private router: Router) {
 
+  }
+
+  toggleCollapsed() {
+    this.collapsed = !this.collapsed;
   }
 
   logout() {
